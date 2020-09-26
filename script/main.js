cc4700508fca2e81c43fb300b8e01ae1add350e5
jvpelai <jv.pelai@gmail.com>
2020-09-26 19:06:17 -0300

    refactored

--- script/main.js
+++ script/main.js
@@ -1,15 +1,4 @@
-var gameboard = (function () {
-  let row1 = ["#", "#", "#"];
-  let row2 = ["#", "#", "#"];
-  let row3 = ["#", "#", "#"];
-  return {
-    row1,
-    row2,
-    row3,
-  };
-})();
-
-const dom_elems = (function () {
+const gameboard = (function () {
   const board = document.querySelector(".game_board");
   const board_row = document.querySelector(".board_tr");
   const board_td = document.querySelectorAll(".board_td").forEach((item) => {
