8433f1c4df61c840ef53e359eb21b27d146367df
jvpelai <jv.pelai@gmail.com>
2020-09-26 19:04:21 -0300

    board and onclick events added

--- script/main.js
+++ script/main.js
@@ -1,10 +1,21 @@
-let gameboard = (function () {
-  let row1 = ["#", "#", "#"]
-  let row2 = ["#", "#", "#"]
-  let row3 = ["#", "#", "#"]
+var gameboard = (function () {
+  let row1 = ["#", "#", "#"];
+  let row2 = ["#", "#", "#"];
+  let row3 = ["#", "#", "#"];
   return {
     row1,
     row2,
-    row3
-  }
+    row3,
+  };
+})();
+
+const dom_elems = (function () {
+  const board = document.querySelector(".game_board");
+  const board_row = document.querySelector(".board_tr");
+  const board_td = document.querySelectorAll(".board_td").forEach((item) => {
+    item.addEventListener("click", function () {
+      this.textContent = "X";
+    });
+  });
+  return { board, board_row, board_td };
 })();
